e29ad8e4c4a5f907b93e1c8340f2e63e185571a0
Daniel Eckelt <crymis8@gmail.com>
2026-01-20 16:39:50 +0100

    feat: add shared banner for current exercise

---
+++ shared/ExerciseBanner.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  number: string;
+}>();
+</script>
+
+<template>
+  <div class="exercise-banner">
+    <p>Exercise {{ props.number }}: {{ props.title }}</p>
+  </div>
+</template>
+
+<style scoped>
+.exercise-banner {
+  background-color: #007acc;
+  color: white;
+  padding: 1rem;
+  border: 2px solid #005f99;
+  text-align: left;
+  font-weight: bold;
+  position: fixed;
+  width: 100vw;
+  top: 0;
+}
+</style>
